a79ff6fc61e9eda787d0cbfa60d6aa05dc01955e
onewaynehart <39324031+onewaynehart@users.noreply.github.com>
2018-05-19 09:44:35 -0400

    Code cleanup
    Refactoring of the settings activity

--- app/src/main/java/com/yourharts/www/bloodglucosetracker/SettingsActivity.java
+++ app/src/main/java/com/yourharts/www/bloodglucosetracker/SettingsActivity.java
@@ -1,68 +1,22 @@
 package com.yourharts.www.bloodglucosetracker;
-
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
-
 import com.yourharts.www.Database.DBHelper;
 import com.yourharts.www.Models.DataModelInterface;
-
 import java.util.List;
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
+
 public class SettingsActivity extends AppCompatPreferenceActivity {
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-
-
-    /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
-
-
     }
-
     public static  class MainPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         private ListPreference _glucoseUnitsLP;
         private ListPreference _correctiveDrugTypeLP;
@@ -80,10 +34,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             _correctiveDrugTypeLP = (ListPreference) findPreference(getString(R.string.defaultCorrectiveDrugID));
             _baselineDrugTypeLP = (ListPreference) findPreference(getString(R.string.defaultBaselineDrugID)) ;
             setDefaults();
-
-
         }
-
         private void setDefaults() {
             if(_dbHelper == null){
                 _dbHelper = new DBHelper(getActivity(), getActivity().getFilesDir().getPath());
@@ -160,7 +111,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
         private  void updateListPrefSummary_PREF_LIST(ListPreference preference){
             CharSequence entry = preference.getEntry();
-            preference.setSummary("Units: " + entry);
+            preference.setSummary("Current selection: " + entry);
         }
 
         @Override
